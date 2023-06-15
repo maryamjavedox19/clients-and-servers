7ba8d4c691666cf91c8d90e38cbe33f9d9942dc5
Maryam <maryamjaved.xo@gmail.com>
2023-06-15 15:19:33 +0500

    npm packages-lodash

--- servers.js
+++ servers.js
@@ -1,9 +1,21 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 
 const server = http.createServer((req, res) => {    //this is going to send it to home page when request is made
-  console.log(req.url, req.method);
+   // lodash
+   const num = _.random(0, 20);
+   console.log(num);
+
+
+   const greet = _.once(() => {
+    console.log('hello');
+  });
+  greet();
+  greet();
+
+  
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
